65c549c74b81e40a81b20e2dfdad45b7eeed4920
Nazima Ergeshova <ergeshovanazima1@gmail.com>
2025-05-30 15:11:20 +0900

    Chapter-12: example03.txt 파일 읽기 예제 Example04.java 추가

---
+++ chapter-12/Example04.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileInputStream;
+
+public class Example04 {
+    public static void main(String[] args) {
+        try {
+            // 읽을 파일 객체 생성
+            File file = new File("example03.txt");
+            // 파일이 존재하지 않으면 새로 생성
+            if (!file.exists())
+                file.createNewFile();
+
+            // 파일을 바이트 단위로 읽기 위한 FileInputStream 생성
+            FileInputStream fis = new FileInputStream(file);
+            int i = 0;
+
+            // 파일 끝(-1)까지 한 바이트씩 읽으며 출력
+            while ((i = fis.read()) != -1) {
+                System.out.print((char) i);
+            }
+            // 스트림 닫기
+            fis.close();
+            System.out.println("\n파일 읽기 성공");
+        } catch (Exception e) {
+            // 예외 발생 시 메시지 출력
+            System.out.println(e);
+        }
+    }
+}
+
